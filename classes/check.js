04cb7025cf88a51ef15c5eac258acc91cb93d2ad
Bruno Vicente <brunovicente.dev@gmail.com>
2023-12-10 11:56:53 -0300

    Added tip

--- classes/check.js
+++ classes/check.js
@@ -1,9 +1,10 @@
 class Check {
-  constructor(restaurant, price, date, people) {
+  constructor(restaurant, price, date, people, tip) {
     this.restaurant = restaurant;
     this.price = price;
     this.date = date;
     this.people = [people];
+    this.tip = tip;
   }
 }
 
