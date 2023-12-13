c4a689286145a569fc122ac4fe55aaaff97f6573
Bruno Vicente <brunovicente.dev@gmail.com>
2023-12-13 14:05:46 -0300

    Deleted console.log

--- public/scripts/switches.js
+++ public/scripts/switches.js
@@ -1,6 +1,5 @@
 const activeLang = (lang) => {
   const switches = document.querySelectorAll(".switches");
-  console.log(lang);
   switches.forEach((checkbox) => {
     if (checkbox.name == lang) {
       checkbox.checked = true;
