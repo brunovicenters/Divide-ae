17248c45a971c3348895b6ce9a6b19a23cbfc67f
Bruno Vicente <brunovicente.dev@gmail.com>
2023-12-13 14:24:45 -0300

    Using id to verify input

--- public/scripts/switches.js
+++ public/scripts/switches.js
@@ -1,7 +1,7 @@
 const activeLang = (lang) => {
   const switches = document.querySelectorAll(".switches");
   switches.forEach((checkbox) => {
-    if (checkbox.name == lang) {
+    if (checkbox.id == lang) {
       checkbox.checked = true;
     } else {
       checkbox.checked = false;
