2e101ece14f9756588477f1fe5249fdbbfdbabff
Bruno Vicente <brunovicente.dev@gmail.com>
2023-12-13 00:53:36 -0300

    Edit person route added

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ const brlCurrency = new Intl.NumberFormat("pt-BR", {
 
 const getDate = () => {
   const d = new Date();
-  let h = d.getHours();
+  let h =
+    d.getHours().toString().length == 1 ? "0" + d.getHours() : d.getHours();
   let min =
     d.getMinutes().toString().length == 1
       ? "0" + d.getMinutes()
@@ -121,6 +122,37 @@ app.post("/addPerson/:arrPos", (req, res) => {
     check.people.push(
       new Person(person.name, new Order(person.dish, person.qty, person.price))
     );
+    checks.push(check);
+    store.set("checks", checks);
+  } catch (error) {
+    console.log(error);
+  }
+  res.redirect("/check/" + (checks.length - 1));
+});
+
+app.put("/editPerson/:arrPos/:personPos", (req, res) => {
+  const checks = store.get("checks");
+  const check = checks[req.params.arrPos];
+  const person = check.people[req.params.personPos];
+  const p = req.body.person;
+  const d = req.body.dish;
+  try {
+    checks.splice(req.params.arrPos, 1);
+    check.date = getDate();
+    person.name = p.name;
+    let dishPos = 0;
+    person.orders.forEach((order) => {
+      if (Array.isArray(d.food)) {
+        order.food = d.food[dishPos];
+        order.quantity = d.qty[dishPos];
+        order.price = d.price[dishPos];
+        dishPos++;
+      } else {
+        order.food = d.food;
+        order.quantity = d.qty;
+        order.price = d.price;
+      }
+    });
     checks.push(check);
     store.set("checks", checks);
   } catch (error) {
