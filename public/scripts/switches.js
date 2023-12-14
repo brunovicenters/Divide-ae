004354632b06c0d238eb98c11aa473fa0d7c0f1c
Bruno Vicente <brunovicente.dev@gmail.com>
2023-12-14 16:28:28 -0300

    Added MultiLanguage

--- public/scripts/switches.js
+++ public/scripts/switches.js
@@ -1,5 +1,18 @@
 const activeLang = (lang) => {
   const switches = document.querySelectorAll(".switches");
+  const select = document.getElementById("languages");
+  const options = select.options;
+  let event = new Event("change");
+
+  for (let i = 0; i < options.length; i++) {
+    if (options[i].value == lang) {
+      options[i].selected = true;
+      select.dispatchEvent(event);
+    } else {
+      options[i].selected = false;
+    }
+  }
+
   switches.forEach((checkbox) => {
     if (checkbox.id == lang) {
       checkbox.checked = true;
@@ -7,4 +20,14 @@ const activeLang = (lang) => {
       checkbox.checked = false;
     }
   });
+
+  const translator = new MultiLanguage();
+
+  translator.registerSelect(document.getElementById("languages"));
+
+  const json = JSON.parse(
+    document.getElementById("translation-sheet").innerHTML
+  );
+
+  translator.addSheet(json);
 };
