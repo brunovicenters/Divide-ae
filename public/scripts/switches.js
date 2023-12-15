18cc322785cc11dde634428b2f97e5bab9c27cc1
Bruno Vicente <brunovicente.dev@gmail.com>
2023-12-14 21:33:51 -0300

    Added currencies

--- public/scripts/switches.js
+++ public/scripts/switches.js
@@ -1,3 +1,27 @@
+const usdCurrency = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 2,
+});
+
+const brlCurrency = new Intl.NumberFormat("pt-BR", {
+  style: "currency",
+  currency: "BRL",
+  minimumFractionDigits: 2,
+});
+
+const eurCurrency = new Intl.NumberFormat("en-DE", {
+  style: "currency",
+  currency: "EUR",
+  minimumFractionDigits: 2,
+});
+
+const gbpCurrency = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "GBP",
+  minimumFractionDigits: 2,
+});
+
 const activeLang = (lang) => {
   const switches = document.querySelectorAll(".switches");
   const select = document.getElementById("languages");
