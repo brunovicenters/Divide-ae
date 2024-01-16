a1f91a0bb4912b9473820c5488a2fbc0bf4b3caa
Bruno Vicente <brunovicente.dev@gmail.com>
2024-01-15 22:42:53 -0300

    Changed theme to dark

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(methodOverride("_method"));
 
 store.set("checks", []);
-store.set("theme", "light");
+store.set("theme", "dark");
 store.set("language", "en");
 
 // DELETE LATER
