ee0e61063f1db8e026cf433a50e3e7d82a324dd9
Bruno Vicente <brunovicente.dev@gmail.com>
2024-02-06 17:36:09 -0300

    Fixed validation for different groups

--- public/scripts/validateCheckbox.js
+++ public/scripts/validateCheckbox.js
@@ -1,12 +1,46 @@
 function validateCheckbox(type) {
-  let checkBoxesGroup = document.querySelectorAll(".sideDishPeople");
-
+  let checkBoxesGroup;
   let atLeastOneChecked = false;
-  checkBoxesGroup.forEach((checkbox) => {
-    if (checkbox.checked) {
-      atLeastOneChecked = true;
+  if (type === 0) {
+    checkBoxesGroup = document.querySelectorAll(".sideDishPeopleAdd");
+
+    checkBoxesGroup.forEach((checkbox) => {
+      if (checkbox.checked) {
+        atLeastOneChecked = true;
+      }
+    });
+  } else {
+    let validationNum = document.querySelector("#checkboxValidationNum");
+
+    validationNum = parseInt(validationNum.innerText);
+
+    console.log(validationNum);
+
+    for (let i = 0; i < validationNum; i++) {
+      let j = 0;
+      let search = `.sideDishPeopleEdit${i}`;
+      checkBoxesGroup = document.querySelectorAll(search);
+      console.log(checkBoxesGroup);
+      checkBoxesGroup.forEach((checkbox) => {
+        if (checkbox.checked) {
+          j++;
+          console.log(j);
+        }
+      });
+      if (j == 0) {
+        if (type === 0) {
+          alert("Please select at least one checkbox from the side dish!");
+        } else {
+          alert("Please select at least one checkbox from each side dish!");
+        }
+        return false;
+      } else {
+        atLeastOneChecked = true;
+      }
     }
-  });
+  }
+
+  console.log("hello world");
 
   if (!atLeastOneChecked) {
     if (type === 0) {
